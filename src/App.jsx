68a76e1994ee910182874253e9b6a8ad9e1d3c5f
Dominque Piffardi <domiskp@gmail.com>
2023-10-16 14:02:01 -0300

    Header agregado por Dominique

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Registro from "./components/Registro";
+import Header from "./components/Header";
 import "./app.css";
 const App = () => {
-	return (
-		<div>
-			<Registro></Registro>
-		</div>
-	);
+  return (
+    <div>
+      <Header />
+      <Registro></Registro>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="header">
+      <h1>Header creado por Dominique</h1>
+    </header>
+  );
+}
+
+export default Header;
